6204c74313ac3d69197b3e04d6f00fbb47c35f7d
mmb L <mammothb+git@gmail.com>
2021-05-21 15:55:45 +0800

    updated syndata generation default setting, added utility notebook to remove identify scraped imageds, updated transfer data and foreground segmentation notebooks

--- defaults.py
+++ defaults.py
@@ -11,7 +11,7 @@ DISTRACTOR_GLOB_STRING = "*.jpg"
 INVERTED_MASK = True  # Set to true if white pixels represent background
 
 # Parameters for generator
-NUMBER_OF_WORKERS = 8
+NUMBER_OF_WORKERS = 6
 BLENDING_LIST = ["box", "gaussian", "motion", "none", "poisson"]
 
 # Parameters for images
